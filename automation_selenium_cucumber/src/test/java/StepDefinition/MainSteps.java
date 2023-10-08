6216cb24565c0cd3aa38f2e48da608a7b58bc06b
ujha777 <ujha777@gmail.com>
2023-10-08 20:27:12 +0530

    Reusability and Paramaterization

--- automation_selenium_cucumber/src/test/java/StepDefinition/MainSteps.java
+++ automation_selenium_cucumber/src/test/java/StepDefinition/MainSteps.java
@@ -10,10 +10,25 @@ public class MainSteps {
 	    // Write code here that turns the phrase above into concrete actions
 	   System.out.println("User landed on netbanking page");
 	}
-	@When("user login to the application")
-	public void user_login_to_the_application() {
+	@When("^user login to the application with (.+) and password (.+)$")
+	public void user_login_to_the_application_with_and_password(String username, String password) {
 	    // Write code here that turns the phrase above into concrete actions
-		 System.out.println("user login to the application");
+		   System.out.println("username:"+username+"password:"+password);
+	}
+//How to achieve parameterization
+	/*
+	 * @When("user login to the application with {string} and password {string} cobination"
+	 * ) public void
+	 * user_login_to_the_application_with_and_password_cobination(String username,
+	 * String password) { // Write code here that turns the phrase above into
+	 * concrete actions
+	 * System.out.println("username:"+username+"password:"+password); }
+	 */
+	//How to achieve use regex to fetch any data type
+	@When("^user login to the application with (.+) and password (.+)$ combination")
+	public void user_login_to_the_application_with_and_password_combination(String username, String password) {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("username:"+username+"password:"+password);
 	}
 	@Then("Home page is displayed")
 	public void home_page_is_displayed() {
--- automation_selenium_cucumber/src/test/java/features/firstprogram.feature
+++ automation_selenium_cucumber/src/test/java/features/firstprogram.feature
@@ -2,8 +2,27 @@
 @tag
 Feature: Application Login
 
-Scenario:Admin page default login
+Scenario: Admin page default login
 Given User is on Netbanking Landing page
-When user login to the application
+When user login to the application with admin and password 1234
 Then Home page is displayed
 And Cards are displayed
+
+Scenario: User page default login
+Given User is on Netbanking Landing page
+When user login to the application with user and password 0953
+Then Home page is displayed
+And Cards are displayed
+
+Scenario: User page default login
+Given User is on Netbanking Landing page
+When user login to the application with "<Username>" and password "<Password>" combination
+Then Home page is displayed
+And Cards are displayed
+
+Examples: 
+	|	Username | Password |
+	| debituser| hello123 |
+	| crediuser| hello456 |
+
+	
